1155a32b91f3501a993371a52fdc9d0df61554b6
“Hferpi” <hferpi@gmail.com>
2026-01-15 18:40:20 +0100

    creacion vistas y rutas

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'Car history') }}</title>
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+</head>
+<body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+    <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6">
+        <!-- Aquí nav, logo, etc -->
+    </header>
+
+    <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
+        <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'home');
+Route::view('/login', 'login');
+Route::view('/register', 'register');
+
+Route::prefix('vehicles')->group(function () {
+    Route::view('/', 'vehicles.index');      // /vehicles
+    Route::view('/create', 'vehicles.create'); // /vehicles/create
+    Route::view('/{id}', 'vehicles.show');   // /vehicles/{id}
 });
+
+Route::prefix('invoices')->group(function () {
+    Route::view('/', 'invoices.index');
+    Route::view('/create', 'invoices.create');
+});
+
+Route::view('/history', 'history');
