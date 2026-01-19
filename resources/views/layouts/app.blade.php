cc04a4431b04940d9b31520cc7a85157164662c8
“Hferpi” <hferpi@gmail.com>
2026-01-19 20:31:07 +0100

    cambios de frontend en login y registro. Etilo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Car history') }}</title>
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
     <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6">
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,12 +1,70 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-@section('content')
-    <h1>Registro</h1>
-    <form method="POST" action="{{ route('register.post') }}">
-        @csrf
-        <input type="text" name="nombre" placeholder="Nombre" required>
-        <input type="email" name="email" placeholder="algo@gmail.com" required>
-        <input type="password" name="password" placeholder="Contraseña" required>
-        <button type="submit">Entrar</button>
-    </form>
-@endsection
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'Car history') }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body
+    class="h-full flex flex-col items-center justify-center p-6 lg:p-8
+            bg-[url(/resources/img/logoBg.png)] bg-bottom-right bg-no-repeat">
+
+   <header class="w-full max-w-[335px] lg:max-w-4xl mb-8 text-center">
+        <h1 class="text-4xl font-bold text-cyan-900 mb-2">
+            Car History Tracker
+        </h1>
+
+        <p class="text-base text-gray-700 mb-1">
+            Lleva un control completo del mantenimiento de tu vehículo.
+        </p>
+
+        <p class="text-sm text-gray-600">
+            Seguimiento de cambios de aceite, neumáticos, reparaciones y facturas,
+            todo en un solo lugar con un tracking claro y eficiente.
+        </p>
+    </header>
+
+    <main class="w-full max-w-[300px] lg:max-w-4xl">
+        <section class="w-full p-6 rounded-2xl  h-[350px] flex justify-center bg-blue-300/70 text-left
+                        flex-col gap-4">
+
+            <h1 class="text-2xl m-2 font-semibold">Registro</h1>
+
+
+            <form method="POST" action="{{ route('register.post') }}"  class="w-60 flex flex-col gap-3">
+                @csrf
+
+                <input type="text" name="nombre" placeholder="Nombre" required
+                    class="rounded-lg border-b-gray-700 border p-2 outline-none">
+
+                <input type="email" name="email" placeholder="algo@gmail.com" required
+                    class="rounded-lg p-2 border-b-gray-700 border outline-none">
+
+                <input type="password" name="password" placeholder="Contraseña" required
+                    class="rounded-lg p-2 border-b-gray-700 border outline-none">
+                    <div class="mt-4 flex gap-2 justify-center w-full">
+                        <button type="submit"
+                            class="w-1/2 border border-cyan-800 rounded-2xl bg-cyan-800 text-white p-2
+                                   transition cursor-pointer hover:bg-[oklch(0.86_0.07_249.31)] hover:text-cyan-800">
+                            Entrar
+                        </button>
+
+                        <a href="{{ route('register') }}" class="w-1/2">
+                            <button type="button"
+                                class="w-full border border-cyan-800 rounded-2xl bg-[oklch(0.86_0.07_249.31)] text-cyan-800 p-2
+                                       transition cursor-pointer hover:bg-cyan-800 hover:text-white">
+                                Regístrate
+                            </button>
+                        </a>
+                    </div>
+            </form>
+
+        </section>
+    </main>
+
+</body>
+
+</html>
